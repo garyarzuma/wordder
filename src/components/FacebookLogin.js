a2f4e0dd7cb21aa0c6007e339b28c4a2030cfb01
garyarzumanyan <garyarzumanyan@gmail.com>
2022-07-20 23:09:10 -0700

    Added icon and styling to facebook sign in button

--- src/components/FacebookLogin.js
+++ src/components/FacebookLogin.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
 import './styles/FacebookLogin.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import {  faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function FbLogin() {
+
+  library.add(fas, faFacebook)
 
   const responseFacebook = (response) => {
     console.log(response)
   }
+
   return (
     <FacebookLogin
       appId={process.env.REACT_APP_FACEBOOK_CLIENT_ID}
@@ -15,7 +22,10 @@ function FbLogin() {
       scope="public_profile,user_friends,email"
       callback={responseFacebook}
       render={renderProps => (
-        <button className="fb-button" onClick={renderProps.onClick}>SIGN IN WITH FACEBOOK</button>
+        <div className='facebook-button-container'>
+          <FontAwesomeIcon icon="fa-brands fa-facebook" className="facebook-icon" inverse />
+          <button className="fb-button" onClick={renderProps.onClick}>Continue with Facebook</button>
+        </div>
       )}
     />
   )
