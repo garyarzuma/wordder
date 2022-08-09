98c2ef3aef54634c1779f810084641e52c402b25
Gary Arzumanyan <garyarzumanyan@gmail.com>
2022-08-09 09:29:53 -0700

    New google button sign in working

--- src/components/GoogleLogin.js
+++ src/components/GoogleLogin.js
@@ -1,19 +1,38 @@
-import GoogleLogin from 'react-google-login'
+//import GoogleLogin from 'react-google-login'
 //import GoogleButton from 'react-google-button'
-import React from 'react'
+import React, { useEffect } from 'react'
 import './styles/GoogleLogin.css'
-import googleIcon from '../images/google-icon.jpg'
-import { useDispatch } from 'react-redux'
-import { setUser } from '../reducers/userReducer'
-import { useNavigate } from 'react-router-dom'
-import statsService from '../services/stats'
+import jwt_decode from 'jwt-decode'
+//import googleIcon from '../images/google-icon.jpg'
+//import { useDispatch } from 'react-redux'
+//import { setUser } from '../reducers/userReducer'
+//import { useNavigate } from 'react-router-dom'
+//import statsService from '../services/stats'
 
 function GoogleLg() {
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const handleCallbackResponse = (response) => {
+    console.log('Encoded JWT ID Token: ' + response.credential)
+    const user = jwt_decode(response.credential)
+    console.log(user)
+  }
 
-  const handleLogin = async googleData => {
+  useEffect(() => {
+    /*global google */
+    google.accounts.id.initialize({
+      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      callback: handleCallbackResponse
+    })
+    google.accounts.id.renderButton(
+      document.getElementById('signInDiv'),
+      { theme: 'outline', size: 'large' }
+    )
+  }, [])
+
+  /*  const dispatch = useDispatch()
+  const navigate = useNavigate()
+ */
+  /*  const handleLogin = async googleData => {
     console.log(googleData)
     const res = await fetch('api/login/v1/auth/google', {
       method: 'POST',
@@ -34,21 +53,24 @@ function GoogleLg() {
 
     dispatch(setUser(null,user.user.name,user.user.picURL,user.user.email))
     navigate('/', { replace: true })
-  }
+  } */
 
   return (
-    <GoogleLogin
-      clientId= {process.env.REACT_APP_GOOGLE_CLIENT_ID}
-      render={renderProps => (
-        <div className='google-button-container'>
-          <img className='google-icon' src={googleIcon}></img>
-          <button className='google-sign-in-button' onClick={renderProps.onClick} disabled={renderProps.disabled}>Sign in with Google</button>
-        </div>
-      )}
-      onSuccess={handleLogin}
-      onFailure={handleLogin}
-      cookiePolicy={'single_host_origin'}
-    />
+    <div>
+      <div id='signInDiv'></div>
+      {/* <GoogleLogin
+        clientId= {process.env.REACT_APP_GOOGLE_CLIENT_ID}
+        render={renderProps => (
+          <div className='google-button-container'>
+            <img className='google-icon' src={googleIcon}></img>
+            <button className='google-sign-in-button' onClick={renderProps.onClick} disabled={renderProps.disabled}>Sign in with Google</button>
+          </div>
+        )}
+        onSuccess={handleLogin}
+        onFailure={handleLogin}
+        cookiePolicy={'single_host_origin'}
+      /> */}
+    </div>
   )
 }
 
