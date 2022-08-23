f48690770df4b0a4fae4c3b34f53d8bbda79ceca
garyarzumanyan <garyarzumanyan@gmail.com>
2022-08-22 23:45:40 -0700

    Updated arrows in rules and solution

--- src/components/Solution.js
+++ src/components/Solution.js
@@ -7,7 +7,7 @@ const Solution = ({ answerArray,toWord }) => {
     <div className="answer-list">
       {answerArray.map(word => {
         return (
-          <div key={word} className="answer">{word.toUpperCase()}----{'>'}</div>
+          <div key={word} className="answer"> {word.toUpperCase()} ➜ </div>
         )
       })}
       <div className="answer" >{toWord.toUpperCase()}</div>
